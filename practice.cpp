d9211fd2e0212241a22f57bab43f8f42fe2a23ba
easy9money <121804998+easy9money@users.noreply.github.com>
2023-01-02 21:23:29 +0800

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARINGS
+#include<stdio.h>
+
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
---
+++ 练习2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a,&b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+	return 0;
+
+}
